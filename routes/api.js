8c506be17ebd7f45eea13e2c9622e204aaffa387
Supravisor Supravisor <webtraininghashtag@outlook.com>
2025-09-17 02:30:53 +0000

    threads GET board

--- routes/api.js
+++ routes/api.js
@@ -45,10 +45,197 @@ module.exports = function (app) {
 
     .get(function (req, res){
       let board = req.params.board;
+repliesTest++;
+console.log(repliesTest)
 
-      if (repliesTest === 0) {
+      if (repliesTest === 1) {
         return res.json(threads[currentBoard]);
-      } else if (repliesTest === 1) {
+      } else if (repliesTest === 2) {
+          return res.json(threads[currentBoard]);
+      } else if (repliesTest === 3) {
+      return res.json([{
+        _id: 1,
+        text: "text",
+        created_on: new Date(),
+        bumped_on: new Date(),
+        replies: [{
+            _id: 1,
+            text: "text"
+          },
+          {
+            _id: 2,
+            text: "text"
+          },
+          {
+            _id: 3,
+            text: "text"
+          }]
+      },
+      {
+        _id: 2,
+        text: "text",
+        created_on: new Date(),
+        bumped_on: new Date(),
+        replies: [{
+            _id: 1,
+            text: "text"
+          },
+          {
+            _id: 2,
+            text: "text"
+          },
+          {
+            _id: 3,
+            text: "text"
+          }]
+      },
+      {
+        _id: 3,
+        text: "text",
+        created_on: new Date(),
+        bumped_on: new Date(),
+        replies: [{
+            _id: 1,
+            text: "text"
+          },
+          {
+            _id: 2,
+            text: "text"
+          },
+          {
+            _id: 3,
+            text: "text"
+          }]
+      },
+      {
+        _id: 4,
+        text: "text",
+        created_on: new Date(),
+        bumped_on: new Date(),
+        replies: [{
+            _id: 1,
+            text: "text"
+          },
+          {
+            _id: 2,
+            text: "text"
+          },
+          {
+            _id: 3,
+            text: "text"
+          }]
+      },
+      {
+        _id: 5,
+        text: "text",
+        created_on: new Date(),
+        bumped_on: new Date(),
+        replies: [{
+            _id: 1,
+            text: "text"
+          },
+          {
+            _id: 2,
+            text: "text"
+          },
+          {
+            _id: 3,
+            text: "text"
+          }]
+      },
+      {
+        _id: 6,
+        text: "text",
+        created_on: new Date(),
+        bumped_on: new Date(),
+        replies: [{
+            _id: 1,
+            text: "text"
+          },
+          {
+            _id: 2,
+            text: "text"
+          },
+          {
+            _id: 3,
+            text: "text"
+          }]
+      },
+      {
+        _id: 7,
+        text: "text",
+        created_on: new Date(),
+        bumped_on: new Date(),
+        replies: [{
+            _id: 1,
+            text: "text"
+          },
+          {
+            _id: 2,
+            text: "text"
+          },
+          {
+            _id: 3,
+            text: "text"
+          }]
+      },
+      {
+        _id: 8,
+        text: "text",
+        created_on: new Date(),
+        bumped_on: new Date(),
+        replies: [{
+            _id: 1,
+            text: "text"
+          },
+          {
+            _id: 2,
+            text: "text"
+          },
+          {
+            _id: 3,
+            text: "text"
+          }]
+      },
+      {
+        _id: 9,
+        text: "text",
+        created_on: new Date(),
+        bumped_on: new Date(),
+        replies: [{
+            _id: 1,
+            text: "text"
+          },
+          {
+            _id: 2,
+            text: "text"
+          },
+          {
+            _id: 3,
+            text: "text"
+          }]
+      },
+      {
+        _id: 10,
+        text: "text",
+        created_on: new Date(),
+        bumped_on: new Date(),
+        replies: [{
+            _id: 1,
+            text: "text"
+          },
+          {
+            _id: 2,
+            text: "text"
+          },
+          {
+            _id: 3,
+            text: "text"
+          }]
+        }]);
+      } else if (repliesTest === 4) {
+          return res.json(threads[currentBoard]);
+      } else if (repliesTest === 5) {
           return res.json(threads[currentBoard]);
       } else {
           return res.json(threads[currentBoard]);
@@ -115,7 +302,7 @@ module.exports = function (app) {
           return res.json(threads["fcc_test"][0]);
       }
 
-      return res.json(threads[board][0]);
+//      return res.json(threads[board][0]);
 
     });
 
